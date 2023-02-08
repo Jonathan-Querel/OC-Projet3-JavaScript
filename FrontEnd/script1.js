9e79dca59a22f4f6e4b3ce9a167b562bda55271c
Jonathan QUEREL <jonathan.querel@gmail.com>
2023-02-08 13:20:50 +0100

    etape1

--- FrontEnd/script1.js
+++
@@ -1,7 +0,0 @@
-const img = document.getElementById('img');
-const node = document.querySelector('figure');
-node.removeChild(node)
-
-fetch('http://localhost:5678/api/works')
-    .then (response => response.json())
-    .then (data => img.src = data[1].imageUrl)
