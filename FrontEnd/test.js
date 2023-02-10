2b620602ad149aa43cccf6d2e78a839996daa71b
Jonathan QUEREL <jonathan.querel@gmail.com>
2023-02-10 15:35:14 +0100

    fetch 1ere etape

--- FrontEnd/test.js
+++ FrontEnd/test.js
@@ -1,30 +1,40 @@
 //Récupération de la class gallery dans une constante
 const galleryContainer = document.querySelector(".gallery");
 console.log(galleryContainer);
+const galleryFigures = Array.from(galleryContainer.children);
+console.log(galleryFigures);
 
 //Suppression des données de la class gallery du fichier HTML
 document.querySelector(".gallery").innerHTML = "";
 
-fetch("http://localhost:5678/api/works")
+const fetchWorks = fetch("http://localhost:5678/api/works");
+
+fetchWorks
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    dataWorks(data);
   })
   .catch((error) => {
     console.error("Il y a eu un problème : " + error);
   });
 
-//Création des éléments
-const figureElement = document.createElement("figure");
-const imgElement = document.createElement("img");
-imgElement.src = "http://localhost:5678/api/works/imageUrl";
-const altElement = document.createElement("alt");
-altElement.innerText = "http://localhost:5678/api/works/title";
-const figcaptionElement = document.createElement("figcaption");
-figcaptionElement.innerText = "http://localhost:5678/api/works/title";
+console.log(fetchWorks);
 
-//Rattachement des balises au DOM
-galleryContainer.appendChild(figureElement);
-figureElement.appendChild(imgElement);
-imgElement.appendChild(altElement);
-figureElement.appendChild(figcaptionElement);
+function dataWorks(works) {
+  let indice = 0;
+  galleryFigures.forEach(() => {
+    const work = works[indice];
+    const figureElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    imgElement.src = work.imageUrl;
+    imgElement.alt = work.title;
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = work.title;
+
+    //Rattachement des balises au DOM
+    galleryContainer.appendChild(figureElement);
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElement);
+    indice++;
+  });
+}
