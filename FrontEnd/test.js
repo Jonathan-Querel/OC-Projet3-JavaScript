c059fb3cf2c01748170af03404dbe0657748ee8b
Jonathan QUEREL <jonathan.querel@gmail.com>
2023-02-11 12:47:02 +0100

    Ajout des boutons catégories

--- FrontEnd/test.js
+++ FrontEnd/test.js
@@ -1,3 +1,34 @@
+//const fetchCategory = fetch("http://localhost:5678/api/categories")
+//.then((response) => response.json())
+//.then((data) => dataCategory(data))
+//.catch((error) => {
+//  console.error("Il y a eu un problème : " + error);
+// });
+
+//console.log(fetchCategory);
+
+//function dataCategory = for (ind = 0 ; ind<dataCategory.length ; ind++) {}
+const mesProjets = document.querySelector("#portfolio");
+const divButton = document.createElement("divbutton");
+divButton.classList.add("bouton");
+mesProjets.appendChild(divButton);
+const buttonElement1 = document.createElement("button");
+buttonElement1.innerText = "Tous";
+buttonElement1.classList.add("btn");
+divButton.appendChild(buttonElement1);
+const buttonElement2 = document.createElement("button");
+buttonElement2.innerText = "Objets";
+buttonElement2.classList.add("btn");
+divButton.appendChild(buttonElement2);
+const buttonElement3 = document.createElement("button");
+buttonElement3.innerText = "Appartements";
+buttonElement3.classList.add("btn");
+divButton.appendChild(buttonElement3);
+const buttonElement4 = document.createElement("button");
+buttonElement4.innerText = "Hôtels & restaurants";
+buttonElement4.classList.add("btn");
+divButton.appendChild(buttonElement4);
+
 //Récupération de la class gallery dans une constante
 const galleryContainer = document.querySelector(".gallery");
 console.log(galleryContainer);
